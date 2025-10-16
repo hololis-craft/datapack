bc91e4fe1d3f6c8ebfa5fddb9ce77debb4d00171
f0reachARR <f0reach@f0reach.me>
2025-10-16 17:56:40 +0900

    Add note block resource pack

---
+++ resourcepack/mkpack.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Directory structure:
+# - mkpack.bash : Creates the resource pack zip files for both Java and Bedrock editions
+# - je/ : Root directory for Java Edition resource pack
+#   - pack.mcmeta : Metadata file for the resource pack
+#   - assets/ : Directory containing all asset files (textures, sounds, etc.)
+# - be/ : Root directory for Bedrock Edition resource pack
+#   - manifest.json : Metadata file for the resource pack
+
+zip -r hololis_craft_je.zip je/
+zip -r hololis_craft_be.mcpack be/
